e4d89347132eed31e88af8c61388473ff9b9f53b
wuhf <wuhf.com>
2019-10-22 19:59:22 +0800

    add BrokenLine

--- brokenline/src/main/java/com/wuwind/brokenline/BrokenLine.java
+++ brokenline/src/main/java/com/wuwind/brokenline/BrokenLine.java
@@ -15,14 +15,16 @@ public class BrokenLine extends View {
     private Paint circlePaint;
     private Paint linePaint;
     private int radius;
-    private int[] value = new int[]{28, 32, 30, 36, 31, 25, 29};
+    private float[] value = new float[]{28, 32, 30, 36, 31, 25, 29};
     private String[] bottomStr = new String[]{"今天", "明天", "星期一", "星期一", "星期一", "星期一", "星期一"};
     private int num;
-    private int max = 0;
-    private int min = 0;
+    private float max = 0;
+    private float min = 0;
     private int textH = 25;
     private String topStr;
     private int padingLeft = 40;
+    private String suffix = "℃";
+
 
     public BrokenLine(Context context) {
         super(context);
@@ -46,7 +48,7 @@ public class BrokenLine extends View {
         if (value == null || value.length < 1)
             return;
         max = min = value[0];
-        for (int i : value) {
+        for (float i : value) {
             if (max < i)
                 max = i;
             if (min > i)
@@ -79,11 +81,11 @@ public class BrokenLine extends View {
         rx += padingLeft;
         int[] point = new int[2];
         for (int i = 0; i < num; i++) {
-            ry = (max - value[i]) * (h - (radius + textH) * 2) / (max - min) + radius + textH;
+            ry = (int) ((max - value[i]) * (h - (radius + textH) * 2) / (max - min) + radius + textH);
             canvas.drawCircle(rx, ry, radius, circlePaint);
             if (i > 0)
                 canvas.drawLine(point[0], point[1], rx, ry, linePaint);
-            topStr = value[i] + "℃";
+            topStr = value[i] + suffix;
             canvas.drawText(bottomStr[i], rx - linePaint.measureText(bottomStr[i]) / 2, h - 5, linePaint);
             canvas.drawText(topStr, rx - linePaint.measureText(topStr) / 2, 15, linePaint);
             point[0] = rx;
@@ -94,5 +96,22 @@ public class BrokenLine extends View {
 
     private void loge(String s) {
         Log.e(BrokenLine.class.getSimpleName(), s);
+    }
+
+    public void setTextSize(int size) {
+        this.linePaint.setTextSize(size);
+    }
+
+    public void setValue(float[] value) {
+        this.value = value;
+        getMinMax();
+    }
+
+    public void setBottomValue(String[] value) {
+        this.bottomStr = value;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
     }
 }
--- brokenline/src/main/java/com/wuwind/brokenline/MainActivity.java
+++ brokenline/src/main/java/com/wuwind/brokenline/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        BrokenLine brokenLine = findViewById(R.id.broken);
+         float[] value = new float[]{28, 44, 56, 77, 31, 25, 29};
+         String[] bottomStr = new String[]{"今天", "星期一", "星期一", "星期一", "星期一", "星期一", "星期一"};
+        brokenLine.setValue(value);
+        brokenLine.setBottomValue(bottomStr);
     }
 
 
